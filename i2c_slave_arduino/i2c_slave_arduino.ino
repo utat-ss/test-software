bf9a38d946ebccc96f562b8ef6a54b40c4458bdf
Bruno Almeida <brunofalmeida@users.noreply.github.com>
2019-02-16 11:56:41 -0500

    Added Arduino I2C slave test program

---
+++ i2c_slave_arduino/i2c_slave_arduino.ino
@@ -0,0 +1,40 @@
+/*
+This program makes the Arduino function as an I2C slave so we can test the OBC SPI to I2C bridge as an I2C master that can send write and read commands.
+
+You need to use an Arduino with I2C support (SDA and SCL pins), e.g. the Arduino Due in the lab.
+
+https://www.arduino.cc/en/reference/wire
+https://www.arduino.cc/en/Tutorial/MasterReader
+https://www.arduino.cc/en/Tutorial/MasterWriter
+*/
+
+#include <Wire.h>
+
+void setup() {
+    // Slave address 8
+    Wire.begin(8);                // join i2c bus with address #8
+    Wire.onRequest(requestEvent); // register event (master read request)
+    Wire.onReceive(receiveEvent); // register event (master write request)
+    Serial.begin(9600);           // start serial for output
+    Serial.print("Started\n");
+}
+
+void loop() {
+    delay(100);
+}
+
+// function that executes whenever data is requested by master
+// this function is registered as an event, see setup()
+void requestEvent() {
+    Wire.write("hello"); // respond with message of 5 bytes
+    // as expected by master
+}
+
+// function that executes whenever data is received from master
+// this function is registered as an event, see setup()
+void receiveEvent(int howMany) {
+    while (Wire.available() > 0) { // loop through all but the last
+        char c = Wire.read(); // receive byte as a character
+        Serial.print(c);         // print the character
+    }
+}
