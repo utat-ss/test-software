9c80596ee22ae27356ee5a3a208bf8581354959e
Bruno Almeida <brunofalmeida@users.noreply.github.com>
2019-02-16 18:15:52 -0500

    Debugged I2C test

--- i2c_slave_arduino/i2c_slave_arduino.ino
+++ i2c_slave_arduino/i2c_slave_arduino.ino
@@ -6,26 +6,31 @@ You need to use an Arduino with I2C support (SDA and SCL pins), e.g. the Arduino
 https://www.arduino.cc/en/reference/wire
 https://www.arduino.cc/en/Tutorial/MasterReader
 https://www.arduino.cc/en/Tutorial/MasterWriter
+
+**THE WIRE LIBRARY USES 7-BIT ADDRESSES**
 */
 
 #include <Wire.h>
 
 void setup() {
+    Serial.begin(9600);           // start serial for output
+    Serial.print("Started\n");
+  
     // Slave address 8
     Wire.begin(8);                // join i2c bus with address #8
     Wire.onRequest(requestEvent); // register event (master read request)
     Wire.onReceive(receiveEvent); // register event (master write request)
-    Serial.begin(9600);           // start serial for output
-    Serial.print("Started\n");
+    
 }
 
 void loop() {
-    delay(100);
+    delay(1);
 }
 
 // function that executes whenever data is requested by master
 // this function is registered as an event, see setup()
 void requestEvent() {
+  Serial.print("request");
     Wire.write("hello"); // respond with message of 5 bytes
     // as expected by master
 }
@@ -33,6 +38,7 @@ void requestEvent() {
 // function that executes whenever data is received from master
 // this function is registered as an event, see setup()
 void receiveEvent(int howMany) {
+  Serial.print("receive");
     while (Wire.available() > 0) { // loop through all but the last
         char c = Wire.read(); // receive byte as a character
         Serial.print(c);         // print the character
